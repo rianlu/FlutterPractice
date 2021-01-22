104a1a374f4ca7439f99b310d1133402c9223d35
Wu Zuolu <wuzuolu@enjoymusic.ai>
2021-01-22 11:59:32 +0800

    add async demo

---
+++ async_demo/README.md
@@ -0,0 +1,7 @@
+# async_demo
+
+## 目录
+
+`main.dart`
+
+[runOnUiThread 在Flutter中等价于什么](https://flutterchina.club/flutter-for-android/#runonuithread-%E5%9C%A8flutter%E4%B8%AD%E7%AD%89%E4%BB%B7%E4%BA%8E%E4%BB%80%E4%B9%88)
---
+++ async_demo/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(new SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => new _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  Map widgets = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sample App"),
+        ),
+        body: ListView.builder(
+            itemCount: widgets["data"].length,
+            itemBuilder: (BuildContext context, int position) {
+              return getRow(position);
+            }));
+  }
+
+  Widget getRow(int i) {
+    return new Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Container(
+            margin: EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Text('${widgets["data"][i]["id"]}'),
+                Expanded(
+                  child: Text(
+                    '${widgets["data"][i]["name"]}',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                Text('${widgets["data"][i]["order"]}'),
+                Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ],
+            )));
+  }
+
+  loadData() async {
+    String dataURL = "https://wanandroid.com/wxarticle/chapters/json";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      widgets = jsonDecode(response.body);
+    });
+  }
+}
